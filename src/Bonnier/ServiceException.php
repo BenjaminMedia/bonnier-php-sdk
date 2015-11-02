1773ccdb9c7dc9f2ab06f91e1de006d4092e01ab
Simon Sessingø <simon.sessingoe@gmail.com>
2015-11-02 15:31:21 +0100

    [TASK] Cleaned up ServiceException.

--- src/Bonnier/ServiceException.php
+++ src/Bonnier/ServiceException.php
@@ -3,18 +3,4 @@ namespace Bonnier;
 
 use Pecee\Http\Rest\RestException;
 
-class ServiceException extends RestException {
-
-	protected $httpResponse;
-
-	public function __construct($message, $code = 0, HttpResponse $httpResponse = null) {
-		parent::__construct($message , $code);
-
-		$this->httpResponse = $httpResponse;
-	}
-
-	public function getHttpResponse() {
-		return $this->httpResponse;
-	}
-
-}
+class ServiceException extends RestException { }
