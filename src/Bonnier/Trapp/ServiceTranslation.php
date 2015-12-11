f4110583efbf259c1d000833da0be554dae80e5b
Nicklas Kevin Frank <nicklas@monori.dk>
2015-12-11 16:05:32 +0100

    Updated ServiceTranslation to have getFieldBySharedKey function

--- src/Bonnier/Trapp/ServiceTranslation.php
+++ src/Bonnier/Trapp/ServiceTranslation.php
@@ -9,111 +9,124 @@ use Bonnier\ServiceException;
 use Bonnier\Trapp\Translation\TranslationCollection;
 use Bonnier\Trapp\Translation\TranslationRevision;
 
-class ServiceTranslation extends RestItem {
+class ServiceTranslation extends RestItem
+{
 
-	const TYPE = 'translation';
+    const TYPE = 'translation';
 
-	public function __construct($username, $secret) {
-		parent::__construct(new ServiceBase($username, $secret, self::TYPE));
-		$this->service->setServiceEventListener($this);
-		$this->service->getHttpRequest()->setPostJson(true);
-		$this->row->fields = array();
-		$this->row->translate_into = array();
-	}
+    public function __construct($username, $secret)
+    {
+        parent::__construct(new ServiceBase($username, $secret, self::TYPE));
+        $this->service->setServiceEventListener($this);
+        $this->service->getHttpRequest()->setPostJson(true);
+        $this->row->fields = array();
+        $this->row->translate_into = array();
+    }
 
-    public function getId() {
+    public function getId()
+    {
         return $this->id;
     }
 
-	/**
-	 * Get translation by id
-	 *
-	 * @param int $id
-	 * @throws \Bonnier\ServiceException
-	 * @return self
-	 */
-	public function getById($id) {
-		if(is_null($id)) {
-			throw new ServiceException('Invalid argument for parameter $id');
-		}
+    /**
+     * Get translation by id
+     *
+     * @param int $id
+     * @throws \Bonnier\ServiceException
+     * @return self
+     */
+    public function getById($id)
+    {
+        if (is_null($id)) {
+            throw new ServiceException('Invalid argument for parameter $id');
+        }
 
-		return $this->api($id);
-	}
+        return $this->api($id);
+    }
 
 
     // TODO: possibly rename to toArray() or delete if unused?
-	public function getPostData() {
-		$row = (array)$this->getRow();
-		return $row;
-	}
+    public function getPostData()
+    {
+        $row = (array)$this->getRow();
+        return $row;
+    }
 
-	/**
-	 * Update translation
-	 *
-	 * @throws \Bonnier\ServiceException
-	 * @return self
-	 */
-	public function update() {
-		$this->row = $this->api($this->id, RestBase::METHOD_PUT, $this->getPostData())->getRow();
-		return $this;
-	}
+    /**
+     * Update translation
+     *
+     * @throws \Bonnier\ServiceException
+     * @return self
+     */
+    public function update()
+    {
+        $this->row = $this->api($this->id, RestBase::METHOD_PUT, $this->getPostData())->getRow();
+        return $this;
+    }
 
-	/**
-	 * Save translation
-	 *
-	 * @throws \Bonnier\ServiceException
-	 * @return self
-	 */
-	public function save() {
-		$this->row = $this->api($this->id, RestBase::METHOD_POST, $this->getPostData())->getRow();
-		return $this;
-	}
+    /**
+     * Save translation
+     *
+     * @throws \Bonnier\ServiceException
+     * @return self
+     */
+    public function save()
+    {
+        $this->row = $this->api($this->id, RestBase::METHOD_POST, $this->getPostData())->getRow();
+        return $this;
+    }
 
 
-	public function onCreateCollection() {
-		return new TranslationCollection($this->service);
-	}
+    public function onCreateCollection()
+    {
+        return new TranslationCollection($this->service);
+    }
 
-	/**
-	 * @return self
-	 */
-	public function onCreateItem(){
-		$self = new self($this->service->getUsername(), $this->service->getSecret());
-		$self->setService($this->service);
-		return $self;
-	}
+    /**
+     * @return self
+     */
+    public function onCreateItem()
+    {
+        $self = new self($this->service->getUsername(), $this->service->getSecret());
+        $self->setService($this->service);
+        return $self;
+    }
 
-	/**
-	 * Get queryable translation collection.
-	 *
-	 * @return TranslationCollection
-	 */
-	public function getCollection() {
-		return $this->onCreateCollection();
-	}
+    /**
+     * Get queryable translation collection.
+     *
+     * @return TranslationCollection
+     */
+    public function getCollection()
+    {
+        return $this->onCreateCollection();
+    }
 
-	public function setDevelopment($bool) {
-		$this->service->setDevelopment($bool);
-		return $this;
-	}
+    public function setDevelopment($bool)
+    {
+        $this->service->setDevelopment($bool);
+        return $this;
+    }
 
-	/**
-	 * Get locale for the original item
-	 *
-	 * @return string
-	 */
-	public function getLocale() {
-		return $this->row->locale;
-	}
+    /**
+     * Get locale for the original item
+     *
+     * @return string
+     */
+    public function getLocale()
+    {
+        return $this->row->locale;
+    }
 
-	/**
-	 * Set the locale for the original item
-	 *
-	 * @param string $locale
-	 */
-	public function setLocale($locale) {
-		$this->row->locale = $locale;
-	}
+    /**
+     * Set the locale for the original item
+     *
+     * @param string $locale
+     */
+    public function setLocale($locale)
+    {
+        $this->row->locale = $locale;
+    }
 
 
     /**
@@ -121,15 +134,16 @@ class ServiceTranslation extends RestItem {
      *
      * @return array(TranslationLanguage)
      */
-    public function getRelatedTranslations() {
+    public function getRelatedTranslations()
+    {
         $out = array();
-        if(isset($this->row->related_translations) && count($this->row->related_translations)) {
-            foreach($this->row->related_translations as $language) {
+        if (isset($this->row->related_translations) && count($this->row->related_translations)) {
+            foreach ($this->row->related_translations as $language) {
                 $out[] = TranslationLanguage::fromArray($language);
             }
         }
         return $out;
-	}
+    }
 
     /**
      * Add language for the item to be translated into
@@ -137,7 +151,8 @@ class ServiceTranslation extends RestItem {
      * @param string $locale a valid locale ie: da_dk, sv_se, ect.
      * @return ServiceTranslation
      */
-    public function addTranslatation($locale) {
+    public function addTranslatation($locale)
+    {
         $this->row->translate_into[] = $locale;
         return $this;
     }
@@ -148,7 +163,8 @@ class ServiceTranslation extends RestItem {
      * @return $this
      * @throws ServiceException
      */
-    public function setTranslateInto(array $locales) {
+    public function setTranslateInto(array $locales)
+    {
         $this->row->translate_into = $locales;
         return $this;
     }
@@ -158,7 +174,8 @@ class ServiceTranslation extends RestItem {
      *
      * @return array(TranslationLanguage)
      */
-    public function getTranslateInto() {
+    public function getTranslateInto()
+    {
         return $this->row->translate_into;
     }
 
@@ -168,11 +185,12 @@ class ServiceTranslation extends RestItem {
      * @param string $locale the locale to look for
      * @return array
      */
-    public function hasTranslation($locale) {
+    public function hasTranslation($locale)
+    {
         if (isset($this->row->related_translations) && count($this->row->related_translations)) {
             /** @var TranslationLanguage $translation */
-            foreach($this->row->related_translations as $translation) {
-                if(TranslationLanguage::fromArray($translation)->getLocale() === $locale) {
+            foreach ($this->row->related_translations as $translation) {
+                if (TranslationLanguage::fromArray($translation)->getLocale() === $locale) {
                     return true;
                 }
             }
@@ -187,18 +205,10 @@ class ServiceTranslation extends RestItem {
      * @param string $comment
      * @return self
      */
-    public function setComment($comment) {
-		$this->row->comment = $comment;
-		return $this;
-	}
-
-    /**
-     * Set comment
-     *
-     * @return string Comment
-     */
-    public function getComment() {
-        return  $this->row->comment;
+    public function setComment($comment)
+    {
+        $this->row->comment = $comment;
+        return $this;
     }
 
     /**
@@ -206,8 +216,9 @@ class ServiceTranslation extends RestItem {
      *
      * @return string Comment
      */
-    public function getOriginalId() {
-        return  $this->row->original_entity_id;
+    public function getComment()
+    {
+        return $this->row->comment;
     }
 
     /**
@@ -215,7 +226,18 @@ class ServiceTranslation extends RestItem {
      *
      * @return string Comment
      */
-    public function isOriginal() {
+    public function getOriginalId()
+    {
+        return $this->row->original_entity_id;
+    }
+
+    /**
+     * Set comment
+     *
+     * @return string Comment
+     */
+    public function isOriginal()
+    {
         return is_null($this->row->original_entity_id);
     }
 
@@ -225,10 +247,11 @@ class ServiceTranslation extends RestItem {
      * @param string $state
      * @return self
      */
-    public function setState($state){
-		$this->row->state = $state;
-		return $this;
-	}
+    public function setState($state)
+    {
+        $this->row->state = $state;
+        return $this;
+    }
 
     /**
      * Add field to be translated
@@ -236,7 +259,8 @@ class ServiceTranslation extends RestItem {
      * @param TranslationField $field
      * @return self
      */
-    public function addField(TranslationField $field) {
+    public function addField(TranslationField $field)
+    {
         $this->row->fields[] = $field->toArray();
         return $this;
     }
@@ -246,15 +270,34 @@ class ServiceTranslation extends RestItem {
      *
      * @return array(TranslationField)
      */
-    public function getFields() {
+    public function getFields()
+    {
         $out = array();
-        if(isset($this->row->fields) && count($this->row->fields)) {
-            foreach($this->row->fields as $field) {
+        if (isset($this->row->fields) && count($this->row->fields)) {
+            foreach ($this->row->fields as $field) {
                 $out[] = TranslationField::fromArray($field);
             }
         }
 
         return $out;
+    }
+
+    /*
+    * Get array of fields
+    *
+    * @return array(TranslationField)
+    */
+    public function getFieldBySharedKey($sharedKey)
+    {
+        if (isset($this->row->fields) && count($this->row->fields)) {
+            foreach ($this->row->fields as $field) {
+                if($field['shared_key'] == $sharedKey){
+                    return TranslationField::fromArray($field);
+                }
+            }
+        }
+
+        return false;
     }
 
 
@@ -265,11 +308,12 @@ class ServiceTranslation extends RestItem {
      * @return self
      * @throws ServiceException
      */
-    public function setFields(array $fields) {
+    public function setFields(array $fields)
+    {
         $newFields = [];
         /** @var \Bonnier\Trapp\Translation\TranslationField $field */
         foreach ($fields as $field) {
-            if (! $field instanceof TranslationField) {
+            if (!$field instanceof TranslationField) {
                 throw new ServiceException('Objects in array passed to setFields() must be instance of TranslationField');
             }
             $newFields[] = $field->toArray();
@@ -284,9 +328,10 @@ class ServiceTranslation extends RestItem {
      *
      * @return \DateTime
      */
-    public function getDeadline() {
-		return new \DateTime($this->row->deadline);
-	}
+    public function getDeadline()
+    {
+        return new \DateTime($this->row->deadline);
+    }
 
 
     /**
@@ -295,37 +340,42 @@ class ServiceTranslation extends RestItem {
      * @param \DateTime $datetime
      * @return $this
      */
-    public function setDeadline(\DateTime $datetime) {
-		$this->row->deadline = $datetime->format(DATE_W3C);
-		return $this;
-	}
+    public function setDeadline(\DateTime $datetime)
+    {
+        $this->row->deadline = $datetime->format(DATE_W3C);
+        return $this;
+    }
 
     /**
      * Returns the title
      *
      * @return string title
      */
-    public function getTitle() {
-		return $this->row->title;
-	}
+    public function getTitle()
+    {
+        return $this->row->title;
+    }
 
 
-	public function setTitle($title) {
-		$this->row->title = $title;
-		return $this;
-	}
+    public function setTitle($title)
+    {
+        $this->row->title = $title;
+        return $this;
+    }
 
     /**
      * Returns the app_code
      *
      * @return string app_code
      */
-    public function getAppCode() {
+    public function getAppCode()
+    {
         return $this->row->app_code;
     }
 
 
-    public function setAppCode($appCode) {
+    public function setAppCode($appCode)
+    {
         $this->row->app_code = $appCode;
         return $this;
     }
@@ -335,37 +385,41 @@ class ServiceTranslation extends RestItem {
      *
      * @return string brand_code
      */
-    public function getBrandCode() {
+    public function getBrandCode()
+    {
         return $this->row->brand_code;
     }
 
 
-    public function setBrandCode($brandCode) {
+    public function setBrandCode($brandCode)
+    {
         $this->row->brand_code = $brandCode;
         return $this;
     }
 
 
     /**
-	 * @return ServiceBase
-	 */
-	public function getService() {
-		return parent::getService();
-	}
+     * @return ServiceBase
+     */
+    public function getService()
+    {
+        return parent::getService();
+    }
 
-	/**
-	 * Create new object from callback response.
-	 *
-	 * @param string $username
-	 * @param string $secret
-	 * @param \stdClass $response
-	 *
-	 * @return static
-	 */
-	public static function fromCallback($username, $secret, \stdClass $response) {
-		$translation = new static($username, $secret);
-		$translation->setRow($response);
-		return $translation;
-	}
+    /**
+     * Create new object from callback response.
+     *
+     * @param string $username
+     * @param string $secret
+     * @param \stdClass $response
+     *
+     * @return static
+     */
+    public static function fromCallback($username, $secret, \stdClass $response)
+    {
+        $translation = new static($username, $secret);
+        $translation->setRow($response);
+        return $translation;
+    }
 
 }
